570689a0ba5ae66b6651aa78d42c55867ef34633
Andrew Peabody <andrewpeabody@google.com>
2022-07-06 10:11:43 -0700

    updates for TF modules

--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -73,7 +73,16 @@ output "domains_to_allow" {
 }
 
 output "target_name_server_addresses" {
-  value = ["192.168.0.1", "192.168.0.2"]
+  value = [
+    {
+      ipv4_address    = "192.168.0.1",
+      forwarding_path = "default"
+    },
+    {
+      ipv4_address    = "192.168.0.2",
+      forwarding_path = "default"
+    }
+  ]
 }
 
 output "scc_notification_name" {
